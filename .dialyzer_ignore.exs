2075f84cfbbd5df93d5239917a64f117e8f660b9
chino <46470388+kafuunochino@users.noreply.github.com>
2026-07-31 17:35:38 +0800

    chore: document generated Gettext warning

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,1 +1,6 @@
-[]
+[
+  # Generated Gettext backend data contains an Expo.PluralForms opaque value.
+  # Dialyzer cannot see through the generated module boundary, but Gettext
+  # accepts this value at runtime.
+  {"lib/teslamate_web/gettext.ex", :call_without_opaque}
+]
